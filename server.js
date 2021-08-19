252e214aa2f80f53435b5dde5420c71878820392
Charlie <boy_916@hotmail.com>
2021-08-18 19:36:08 -0700

    got backend completed?

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-// const jwksClient = require('jwks-rsa');
+const mongoose = require('mongoose');
+const BookModel = require('./modals/books.js');
 
 const app = express();
 app.use(cors());
@@ -32,22 +33,108 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+// app.get('/clear', clear);
+
 app.get('/test', (req, res) => {
 
-  // DONE: 
-  // STEP 1: get the jwt from the headers
+  // Using split takes out the word bearer from the front end
   const token = req.headers.authorization.split(' ')[1];
-  // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
-  // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
 
+  // Use the jsonwebtoken library to verify that it is a valid jwt
+  // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
   jwt.verify(token, getKey, {}, function (err, user){
     if(err){
       response.status(500).send('invlaid token');
     }
     res.send(user);
   });
-  // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
 
 })
 
+app.get('/books', async (req, res) => {
+  try {
+    // C READ U D
+    let booksdb = await BookModel.find({});
+    res.status(200).send(booksdb);
+  } catch (err) {
+    res.status(500).send('dbase error');
+  }
+})
+
+// start the db
+// terminal idAddress = ... Port 27017
+// conntect to the db                    Name of DB-not related to books path
+mongoose.connect('mongodb://127.0.0.1:27017/books',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+// .then makes it connect to the DB first 
+// Does not need to be in this promise
+.then( async () => {
+  try {
+  console.log('connected to the database');
+
+  // Seeded Data
+  let books = await BookModel.find({});
+  if (books.length === 0) {
+    await addBook({
+      title: "New Book1",
+      description: "Awesome Author",
+      status: "status",
+      email: "email",
+    })
+    await addBook({
+      title: "New Book2",
+      description: "Awesome Author",
+      status: "status",
+      email: "email",
+    })
+    await addBook({
+      title: "New Book3",
+      description: "Awesome Author",
+      status: "status",
+      email: "email",
+    })
+  }
+
+  // storing a new book single
+  let newBook = new BookModel({
+    title: "Tobin's Spirit Guide",
+    description: "H Thomas Tobin",
+    status: "Status",
+    email: "email",
+  })
+
+  await newBook.save();
+  } catch (error) {
+  console.log('error in mongoose connect', error)
+  }
+
+  app.listen(3007, () => {
+    console.log('Server up on 3001????????');
+  });
+});
+
+
+// Function to add book to DB
+async function addBook(obj){
+  // CREATE R U D
+  // {title: "", author ""}
+  let newBook = new BookModel(obj);
+  return await newBook.save();
+}
+
+// function to clear our DB
+async function clear(){
+  // C R U DELETE
+  try {
+    await BookModel.deleteMany({});
+    console.log('Cleared the whole DB!!!');
+  } catch (err) {
+    console.log('Error in clearing the DB!');
+  }
+}
+
+
+// listen to port
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
